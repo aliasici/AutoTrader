2b77c85f136e6444901f725da0863b413572efd9
aliasici <asiciali@yahoo.com>
2020-12-17 00:10:17 -0800

    I added JavaFaker dependencies

--- src/test/java/com/autotrader/tests/dropdowns/JavaFaker.java
+++ src/test/java/com/autotrader/tests/dropdowns/JavaFaker.java
@@ -1,2 +1,21 @@
-package com.autotrader.tests.dropdowns;public class JavaFaker {
+package com.autotrader.tests.dropdowns;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFaker {
+    @Test
+    public void faker_test(){
+
+        Faker faker = new Faker();
+
+        String firstName = faker.name().firstName();
+        System.out.println("firstName = " + firstName);
+
+        String lastName = faker.name().lastName();
+        System.out.println("lastName = " + lastName);
+
+        String fullName = faker.name().fullName();
+        System.out.println("fullName = " + fullName);
+    }
 }
