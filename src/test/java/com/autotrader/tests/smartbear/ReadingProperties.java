d81cf057d55bce771d04349cb1d94c69032d4c17
aliasici <asiciali@yahoo.com>
2020-12-21 12:46:55 -0800

    I created ReadingProperties class

--- src/test/java/com/autotrader/tests/smartbear/ReadingProperties.java
+++ src/test/java/com/autotrader/tests/smartbear/ReadingProperties.java
@@ -1,2 +1,36 @@
-package com.autotrader.tests.smartbear;public class ReadingProperties {
+package com.autotrader.tests.smartbear;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file() throws IOException {
+        // Properties: Use Properties class object
+        // we create properties object to be able to read configuration.properties
+        //import from java.util
+
+        Properties properties = new Properties();
+        // fileinputstream opens the file
+
+        String path = "configuration.properties";
+
+        // we need to pass the path file we want to open in JVM
+        FileInputStream file = new FileInputStream(path);
+
+        // we will load rhe file
+
+        properties.load(file);
+
+        properties.getProperty("browser");
+
+        file.close();
+
+
+    }
+
 }
