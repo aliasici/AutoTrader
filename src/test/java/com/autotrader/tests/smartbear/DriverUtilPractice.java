c964616cba135566dd9d90ac49dac4e67afa1c6d
aliasici <asiciali@yahoo.com>
2020-12-21 20:32:34 -0800

    I tried driver class with example

--- src/test/java/com/autotrader/tests/smartbear/DriverUtilPractice.java
+++ src/test/java/com/autotrader/tests/smartbear/DriverUtilPractice.java
@@ -1,2 +1,30 @@
-package com.autotrader.tests.smartbear;public class DriverUtilPractice {
+package com.autotrader.tests.smartbear;
+
+import com.autotrader.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DriverUtilPractice {
+
+    @Test
+    public void simple_google_search(){
+
+        Driver.getDriver().get("https://www.google.com");
+
+        WebElement searchbox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+
+        searchbox.sendKeys("apple watch"+ Keys.ENTER);
+
+        String expectedTitle = "apple watch";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+    }
+
+
 }
